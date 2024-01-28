4528fd4c2be1c20b42955e1dd411e156d31d8007
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-01-28 20:12:58 +0530

    Add project files.

---
+++ question 06/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace question06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the student's name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the student's exam marks: ");
+            int marks;
+            if (Int32.TryParse(Console.ReadLine(), out marks))
+            {
+                if (marks >= 0 && marks <= 100)
+                {
+                    if (marks >= 75)
+                    {
+                        Console.WriteLine(name + " has received Grade A.");
+                    }
+                    else if (marks >= 60)
+                    {
+                        Console.WriteLine(name + " has received Grade B.");
+                    }
+                    else if (marks >= 50)
+                    {
+                        Console.WriteLine(name + " has received Grade C.");
+                    }
+                    else if (marks >= 40)
+                    {
+                        Console.WriteLine(name + " has received Grade D.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(name + " has failed.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Invalid marks");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
